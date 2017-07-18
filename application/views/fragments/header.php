97f90e41682d194ab09ff1fb2694fa8d8058a818
Raul Olivares <broobie.raul@gmail.com>
2017-07-18 00:20:04 -0300

    Header * EL bufete tenia nombre :v

--- application/views/fragments/header.php
+++ application/views/fragments/header.php
@@ -34,7 +34,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand">Buffet</a>
+                    <a class="navbar-brand">Duralex bufete</a>
                 </div>
                 <div class="navbar-collapse collapse" id="navopc">
                     <?php if(isset($_SESSION['username'])): ?>
